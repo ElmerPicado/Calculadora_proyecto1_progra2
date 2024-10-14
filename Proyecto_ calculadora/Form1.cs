cfb10f74369f098897bd9b3242b71d95a8ac5fb9
ElmerPicado <epicadomiranda@gmail.com>
2024-10-13 20:36:19 -0600

    Proyecto proga2

---
+++ Proyecto_ calculadora/Form1.cs
@@ -0,0 +1,50 @@
+namespace Proyecto__calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void b_coma_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
