f2a6c6b9e0c60c7e3db91b4a4bf4534a5ab5843c
Enkike1506 <166204521+Enkike1506@users.noreply.github.com>
2024-10-17 10:26:26 -0600

    Create Raiz.cs

---
+++ Proyecto_ calculadora/Raiz.cs
@@ -0,0 +1,8 @@
+namespace Proyecto_calculadora{
+  internal class Raiz{
+    public static float RaizCuadrada(float num){
+      float resultado = Math.Sqrt(num);
+      return resultado;
+    }
+  }
+}
